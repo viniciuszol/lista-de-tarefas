ffbc75bbdb9c9959e2f6fe8fab898d17656276cb
Vinicius Souza <163210316+viniciuszol@users.noreply.github.com>
2024-11-01 20:39:22 -0300

    Add files via upload

---
+++ src/Tarefa.java
@@ -0,0 +1,27 @@
+public class Tarefa {
+  
+  private String descricao;
+  private boolean concluida;
+
+  public Tarefa(String descricao) {
+    this.descricao = descricao;
+    this.concluida = false;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public boolean isConcluida() {
+    return concluida;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public void setConcluida(boolean concluida) {
+    this.concluida = concluida;
+  }
+
+}
